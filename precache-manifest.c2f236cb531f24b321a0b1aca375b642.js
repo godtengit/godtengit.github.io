0b412461dec3ae92bd82c33661659f539fddf671
Teng <john@501.local>
2020-05-16 10:19:33 -0700

    Updates

---
+++ precache-manifest.c2f236cb531f24b321a0b1aca375b642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003e622d6ed08ee38c732352d6b36e40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a6be5525582b6056ae6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc473dbf1f915afd4dd0",
+    "url": "/static/js/2.93807573.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.93807573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6be5525582b6056ae6",
+    "url": "/static/js/main.68d52bdb.chunk.js"
+  },
+  {
+    "revision": "c7ef8fb51890288f75b9",
+    "url": "/static/js/runtime-main.1fcb0c7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
